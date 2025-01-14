8e44e84918dc24cc0d0a2c1d0096a3b159985e52
Polina Katunina <katunina0polina@gmail.com>
2025-01-14 23:08:32 +0300

    add behavior for create entry

--- static/pages/adminpage.tsx
+++ static/pages/adminpage.tsx
@@ -4,45 +4,15 @@ import ToroHeyyy from "./../assets/toro_heyyy.png";
 const apiPath = '/api/v1/entries';
 const apiUrl = window.location.origin + apiPath;
 
+
 export default function AdminPage() {
-    const [entries, setEntries] = useState(new Map<string, string>);
-    useEffect(() => {
-        fetch(apiUrl).then(response => {
-            if (!response.ok) {
-                throw new Error(`network response failed from url ${apiUrl}`);
-            }
-
-            return response.json()
-        }).then(jsonResponse => {
-            const data: Map<string, string> = new Map(Object.entries(jsonResponse['data']));
-            setEntries(data)
-        }).catch(error => {
-            console.error(`api call failed: ${error}`)
-        });
-    }, [])
-
-    let entryList: JSX.Element[] = [];
-        entries.forEach((value, key) => {
-            entryList.push(
-                <tr className="table-element">
-                    <td>
-                        <div className="table-element">{key}</div>
-                    </td>
-                    <td>
-                        <div className="table-element">{value}</div>
-                    </td>
-                </tr>
-        )
-
-    })
-
     return(
          <div className="center">
             <img src={ToroHeyyy}></img>
             <EntriesList></EntriesList>
 
             <div>
-                <button type="button" className="nes-btn" id="add-entry-button">add entry</button>
+                <AddEntry></AddEntry>
                 <button type="button" className="nes-btn">update entry</button>
                 <button type="button" className="nes-btn">delete entry</button>
             </div>
@@ -60,6 +30,11 @@ function EntriesList(): ReactNode {
 
             return response.json()
         }).then(jsonResponse => {
+            const error: string = jsonResponse['error'];
+            if (error != "") {
+                throw new Error(error)
+            }
+
             const data: Map<string, string> = new Map(Object.entries(jsonResponse['data']));
             setEntries(data)
         }).catch(error => {
@@ -96,4 +71,69 @@ function EntriesList(): ReactNode {
                 </table>
             </div>
     ) 
+}
+
+function AddEntry(): ReactNode {
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+    const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const pathElement = e.currentTarget.elements.namedItem("path") as HTMLInputElement | null;
+        const urlToRedirect = e.currentTarget.elements.namedItem("url") as HTMLInputElement| null;
+
+        if (pathElement == null) {
+            console.log("cannot find text element \"path on a site\"")
+            setIsDialogOpen(false)
+            return
+        }
+
+        if (urlToRedirect == null) {
+            console.log("cannot find text element \"url to redirect\"")
+            setIsDialogOpen(false)
+            return
+        }
+
+        urlToRedirect.value
+        const req = await fetch(apiUrl, {
+            method: 'POST',
+            body: JSON.stringify({
+                [urlToRedirect.value]: pathElement.value
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+        const response = await req.json();
+        if (response['error'] != "") {
+            console.error(`post api call failed: ${response['error']}`)
+        }
+
+        window.location.reload()
+    }
+
+    const dialog = (
+        <dialog className="nes-dialog is-dark dialog-box" open>
+            <form method="dialog" onSubmit={submitForm}>
+                <div className="center">add entry</div>
+                 <menu className="dialog-menu">
+                     <input type="text" name="path" className="nes-input is-dark" placeholder="path on a site"></input>
+                     <input type="text" name="url" className="nes-input is-dark" placeholder="url to redirect"></input>
+
+                     <div className="dialog-buttons">
+                         <button type="button" className="nes-btn" onClick={() => setIsDialogOpen(false)}>Cancel</button>
+                         <button type="submit" className="nes-btn is-primary">Confirm</button>
+                     </div>
+                 </menu>
+             </form>
+         </dialog>
+    )
+
+    return(
+        <>
+            <button type="button" className="nes-btn" onClick={() => setIsDialogOpen(true)}>add entry</button>
+            {isDialogOpen ? dialog: null}
+        </>
+    )
 }
